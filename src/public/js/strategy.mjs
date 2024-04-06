df293db7683668d88d501d64207557acc96dc069
Ishan Pranav <ishan.pranav@outlook.com>
2024-04-06 11:33:24 -0400

    Clean up

--- src/public/js/strategy.mjs
+++
@@ -1,9 +0,0 @@
-// strategy.mjs
-// Copyright (c) 2024 Ishan Pranav
-// Licensed under the MIT license.
-
-export class Strategy {
-    play(state) {
-        throw new Error("Not implemented");
-    }
-}
